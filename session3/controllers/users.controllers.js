4098c07a60ee23a5cbf8ff4d0e0c44006c8dbf90
AKASH A BENKI <akash.0.benki74169@gmail.com>
2024-11-24 23:04:05 +0530

    JOI validations

--- session3/controllers/users.controllers.js
+++ session3/controllers/users.controllers.js
@@ -1,4 +1,5 @@
 const usersJson = require("./users.json");
+const { userSearchSchema } = require("../validations/userSearchValidation")
 
 // - Below is the endpoint to get all users
 
@@ -25,22 +26,37 @@ const getUserById = (req, res) => {
 
 // - Below are the endpoints to test for the SearchUsers
 
-// http://localhost:8082/users/search?gender=female
-// http://localhost:8082/users/search?age=50
-// http://localhost:8082/users/search?gender=female&age=30
+// - http://localhost:8082/users/search?gender=female
+// - http://localhost:8082/users/search?age=50
+// - http://localhost:8082/users/search?gender=female&age=30
 
 const searchUsers = (req, res) => {
     const { gender, age } = req.query;
-    const validGenders = ["male", "female"];
+    // const validGenders = ["male", "female"];
 
-    // http://localhost:8082/users/search?gender=15
-    if (gender && !validGenders.includes(gender))
-        return res.status(404).send({ message: "Gender must be either male or female" });
+    // - http://localhost:8082/users/search?gender=male&age=49
+    // - http://localhost:8082/users/search?age=male (Gives "error" key as well in the result object)
+    // const result = userSearchSchema.validate({ gender, age }); // This one or below both are same
+    // const result = userSearchSchema.validate({ gender: gender, age: age });
+    // console.log(result); // { value: { gender: 'male', age: 49 } }
 
-    // http://localhost:8082/users/search?age=male
-    // http://localhost:8082/users/search?age=-5
-    if ((age && isNaN(age)) || age <= 0 || age > 100)
-        return res.status(404).send({ message: "Age is not a number or not within 0 - 100 limit" });
+    const { error } = userSearchSchema.validate({ gender, age });
+    // if (error) {
+    //     return res.status(400).send({ message: "Validation failed", error })
+    // }
+    // - We can also just send the error message
+    if (error) {
+        return res.status(400).send({ message: error.details[0].message })
+    }
+
+    // - http://localhost:8082/users/search?gender=15
+    // if (gender && !validGenders.includes(gender))
+    //     return res.status(404).send({ message: "Gender must be either male or female" });
+
+    // - http://localhost:8082/users/search?age=male
+    // - http://localhost:8082/users/search?age=-5
+    // if ((age && isNaN(age)) || age <= 0 || age > 100)
+    //     return res.status(404).send({ message: "Age is not a number or not within 0 - 100 limit" });
 
     if (gender && age)
         return res.send(
@@ -61,3 +77,17 @@ const searchUsers = (req, res) => {
 };
 
 module.exports = { getUsers, getUserById, searchUsers };
+
+
+
+// - Points to be noted
+/*
+1. Joi Docs - https://joi.dev/api/?v=17.13.3
+
+2.When we have no error in the validation we have only one key called: "value"
+- When we have an error in the validation then we have one more key called: "error"
+
+3. const {error} = useSearchSchema.validate({gender, age});
+- If there is an error it will be present in the above error variable else undefined
+
+*/
